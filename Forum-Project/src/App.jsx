4b638004f6248051ccbaec9f86c13353e5050212
Plamen Milanov <plamenpk@gmail.com>
2023-11-02 23:20:25 +0100

    Add partial publicView

--- Forum-Project/src/App.jsx
+++ Forum-Project/src/App.jsx
@@ -2,10 +2,10 @@ import Home from "./views/Home/Home";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import Login from "./views/Login/Login";
-
 import { Routes, Route } from "react-router-dom";
 import Signup from "./views/Signup/Signup";
 import About from "./views/About/About";
+import PublicView from "./views/PublicView/PublicView";
 
 
 const App = () => {
@@ -13,7 +13,8 @@ const App = () => {
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<PublicView />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/Signup" element={<Signup />} />
         <Route path="/About" element={<About />} />
---
+++ Forum-Project/src/views/PublicView/PublicView.jsx
@@ -0,0 +1,35 @@
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
+
+const PublicView = () => {
+  const [recommendedPosts, setRecommendedPosts] = useState(false);
+  // const setRecommendedPostsHandler = () => setRecommendedPosts()
+  return (
+    <>
+      <div style={{ margin: '10px', padding: "10px", border: "1px solid black" }}>
+        <p>Welcome to the Dynamic Island forums...</p>
+        <p>
+          You are currently viewing our boards as a guest which gives you limited access to view most discussions and access our other features.
+          Only registered members may post questions, contact other members or search our database of over several millions posts.
+          Please Click <NavLink to={`/Login`}>LOGIN</NavLink> or Click to <NavLink to={`/Signup`}>REGISTER</NavLink></p>
+      </div>
+      <div style={{ margin: '10px', padding: "10px", border: "1px solid black" }}>
+        {recommendedPosts ? (
+          <div>
+            <NavLink to={`/recently-added-posts`} onClick={() => setRecommendedPosts(true)}>recently added posts</NavLink>
+            <p>recommended</p>
+          </div>
+        ) : (
+          <div>
+            <NavLink to={`/recommended-posts`}>recommended posts</NavLink>
+            <p>recently added posts</p>
+          </div>
+        )}
+
+
+      </div>
+    </>
+  )
+}
+
+export default PublicView;
