ef3b27c8867d7ccb60184b995d0ab0511d0eebe6
Plamen Milanov <plamenpk@gmail.com>
2023-11-09 19:30:09 +0100

    Add SingleComment

--- Forum-Project/src/components/RecommendedPosts/SingleComment/SingleComment.jsx
+++ Forum-Project/src/components/RecommendedPosts/SingleComment/SingleComment.jsx
@@ -1,15 +1,19 @@
 
-const SingleComment = () => {
+const SingleComment = (props) => {
+
+  const comment = props.value;
   return (
     <div>
-      <div>
-        SingleComment
+      <div className="font-semibold text-gray-900">
+        
+        <div className='pl-3'>{comment.title}</div>
       </div>
-      <div>
-        SingleComment
+      <div className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+        {comment.content}
       </div>
     </div>
   )
 }
 
 export default SingleComment
+
