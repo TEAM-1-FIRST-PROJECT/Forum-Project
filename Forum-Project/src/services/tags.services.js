bd028722376ca0b4688dfcaf5008891fc3a9196d
Plamen Milanov <plamenpk@gmail.com>
2023-11-10 23:36:48 +0100

    Add tagExistChecker

--- Forum-Project/src/services/tags.services.js
+++ Forum-Project/src/services/tags.services.js
@@ -1,22 +1,23 @@
-import { ref, push, update } from 'firebase/database';
+import { ref, push, update, get } from 'firebase/database';
 import { database } from '../config/firebase-config';
+import { toast } from 'react-toastify';
 
-
-export const addTags = (tag, postId) => {
-
-  return push(
-    ref(database, 'tags'),
-    {
-      [tag]: { postId },
-      
-    },
-  )
-}
-
-export const createTag = (tag, postId) => {
+export const tagsUpdateHandler = (tag, postId) => {
   const updateLikes = {};
-  
+
   updateLikes[`/tags/${tag}/${postId}`] = true;
 
   return update(ref(database), updateLikes);
+};
+
+export const tagExistChecker = (tag) => {
+  return get(ref(database, "tags")).then((snapshot) => {
+    if (!snapshot.exists()) {
+      toast.error(`User with searchTerm ${tag} does not exist!`);
+    }
+
+    const tags = snapshot.val();
+
+    return Object.keys(tags).includes(tag);
+  });
 };
