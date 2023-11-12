4735aa663421b2692c6b046e9e585b885be3deb2
Plamen Milanov <plamenpk@gmail.com>
2023-11-12 11:28:47 +0100

    Fix syntax error

--- Forum-Project/src/views/NewPost/NewPost.jsx
+++ Forum-Project/src/views/NewPost/NewPost.jsx
@@ -127,7 +127,7 @@ const NewPost = () => {
               onClick={handlePostSubmit}
               className="px-4 py-2 text-sm font-semibold text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-600"
             >
-              Post
+               SUBMIT
             </button>
           </div>
           {isPostSubmitted && (
