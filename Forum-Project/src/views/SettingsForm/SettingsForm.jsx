853d57ffc93643a3378991cc499b4686fda410c6
TodorT <ttodorov95@icloud.com>
2023-11-13 13:28:57 +0200

    Change the button names for upload profile photo and save changes in settings form

--- Forum-Project/src/views/SettingsForm/SettingsForm.jsx
+++ Forum-Project/src/views/SettingsForm/SettingsForm.jsx
@@ -140,7 +140,7 @@ toast.error(err.message)
                 onClick={handleUpload}
                 className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
               >
-                Change
+                Upload
               </button>
             </div>
           </div>
@@ -247,7 +247,7 @@ toast.error(err.message)
             onClick={handleUpdateUserData}
             className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Save
+            Save Changes
           </button>
         </div>
       </form>
