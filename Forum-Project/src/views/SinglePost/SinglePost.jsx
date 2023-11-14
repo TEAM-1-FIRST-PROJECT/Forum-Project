8e017841e7ba88b464d26781f3f0a96cd527a3be
Plamen Milanov <plamenpk@gmail.com>
2023-11-14 18:29:12 +0100

    Fix Moderator permissions

--- Forum-Project/src/views/SinglePost/SinglePost.jsx
+++ Forum-Project/src/views/SinglePost/SinglePost.jsx
@@ -24,7 +24,7 @@ const SinglePost = (props) => {
   const [img, setImg] = useState('');
   const navigate = useNavigate();
   const { user, userData } = useContext(AuthContext);
-  
+
   const post = props.value;
   const postId = post.id;
 
@@ -34,6 +34,7 @@ const SinglePost = (props) => {
 
   const userName = userData ? userData.username : null;
   const isAuthor = postAuthor === userName;
+  const deletePermission = userData.isModerator || isAuthor;
   useEffect(() => {
     getUserByHandle(post.author)
       .then((snapshot) => {
@@ -108,20 +109,19 @@ const SinglePost = (props) => {
     minute: "2-digit",
   };
   const formattedDate = myDate.toLocaleString("bg-BG", options);
-console.log(post)
-
+  
   return (
- 
-  <article className="flex flex-col min-h-full p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 hover:shadow-indigo-400">
-  <div className="flex justify-between items-center mb-5 text-gray-400">
-    <span className="bg-primary-100 text-black text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-    <div className="flex items-center space-x-4">
-      <img className="w-7 h-7 rounded-full" src={img} alt={`Profile of ${post.author}`} />
-      <span className="font-medium dark:text-white">{post.author}</span>
-    </div>
-    </span>
-    <div className="flex items-center gap-x-4">
-    <div className="flex flex-wrap">
+
+    <article className="flex flex-col min-h-full p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <div className="flex justify-between items-center mb-5 text-gray-400">
+        <span className="bg-primary-100 text-black text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+          <div className="flex items-center space-x-4">
+            <img className="w-7 h-7 rounded-full" src={img} alt={`Profile of ${post.author}`} />
+            <span className="font-medium dark:text-white">{post.author}</span>
+          </div>
+        </span>
+        <div className="flex items-center gap-x-4">
+          <div className="flex flex-wrap">
             <p
               key={post.id}
               className="m-1 px-2 py-1 bg-gray-200 rounded-full text-xs"
@@ -133,14 +133,14 @@ console.log(post)
             {formattedDate}
           </time>
         </div>
-  </div>
-  <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-    <Link to={`/PostDetails/${post.id}`}>{post.title}</Link>
-  </h2>
-  <p className="mb-5 font-light text-gray-500 dark:text-gray-400">{post.content}</p>
-  <div className="flex justify-between items-center">
-  </div>
-  <div className="flex items-center gap-x-4 text-xs mt-4">
+      </div>
+      <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        <Link to={`/PostDetails/${post.id}`}>{post.title}</Link>
+      </h2>
+      <p className="mb-5 font-light text-gray-500 dark:text-gray-400">{post.content}</p>
+      <div className="flex justify-between items-center">
+      </div>
+      <div className="flex items-center gap-x-4 text-xs mt-4">
         {user && <button
           className={`rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100 ${userLike ? 'text-blue-500' : 'text-gray-600'}`}
           onClick={toggleLikePostHandler}
@@ -167,21 +167,20 @@ console.log(post)
         >
           <FaInfoCircle /> Post Details
         </Link>}
-        {isAuthor && <Link
+        {deletePermission && <Link
           to={`/EditPost/${postId}`}
           className="rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
         >
           <FaRegCommentDots /> Edit post
         </Link>}
-        {isAuthor && <button
+        {deletePermission && <button
           className="rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
           onClick={deletePostHandler}
         >
           <FaTrash /> Delete Post
         </button>}
       </div>
-</article>
-
+    </article>
   )
 }
 
